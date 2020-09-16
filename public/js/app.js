caedde14c3514597d321621d7818b57c33d504dd
29886071 <mallikarjunpatil44@gmail.com>
2020-09-16 11:58:41 -0400

    test

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,6 @@ const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('1');
     const location = search.value
 
     messageOne.textContent = 'Loading...'
